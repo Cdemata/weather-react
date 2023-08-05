a0fb32b3d1fba28d172420c93f0e7f49c1532983
Cdemata <chanelle.demata@gmail.com>
2023-08-04 19:53:55 -0700

    added API integration

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Weather from "./Weather.js";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <Weather defaultCity="Tokyo" />
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Weather from "./Weather.js";
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App.js";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Weather />
-    </div>
-  );
-}
+<App />;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
